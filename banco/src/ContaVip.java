27bfbfe3259c17dff618893bf6ddf2a40cf9c075
joaocarloslima <joaocarloslima@me.com>
2023-02-24 23:32:12 -0200

    projeto banco

---
+++ banco/src/ContaVip.java
@@ -0,0 +1,20 @@
+public class ContaVip extends Conta {
+
+    private double limite;
+
+    public ContaVip(int numero, double saldo, double limite) {
+        super(numero, saldo);
+        this.limite = limite;
+    }
+
+    public double calcularSaldo(){
+        return saldo + limite;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    
+    
+}
